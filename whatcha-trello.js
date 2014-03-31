2f43c9a159c8657fac381c4d78b65e45506e9f53
Mark Woodall <mawoodall@gmail.com>
2014-03-30 20:06:10 -0500

    split getMe and getCards out

--- whatcha-trello.js
+++ whatcha-trello.js
@@ -13,28 +13,35 @@ The &dummy=.js part of the managed resource URL is required per http://doc.jsfid
 var onAuthorize = function() {
     updateLoggedIn();
     $("#output").empty();
+    getMe();
+    getCards();
 
-    Trello.members.get("me", function(member){
-        $("#fullName").text(member.fullName);
+};
 
-        var $cards = $("<div>")
-            .text("Loading Cards...")
-            .appendTo("#output");
+var getMe = function() {
+  Trello.members.get("me", function(member){
+    $("#fullName").text(member.fullName);
+  });
 
-        // Output a list of all of the cards that the member
-        // is assigned to
-        Trello.get("members/me/cards", function(cards) {
-            $cards.empty();
-            $.each(cards, function(ix, card) {
-                $("<a>")
-                .attr({href: card.url, target: "trello"})
-                .addClass("card")
-                .text(card.name)
-                .appendTo($cards);
-            });
-        });
-    });
+};
 
+var getCards = function() {
+  var $cards = $("<div>")
+      .text("Loading Cards...")
+      .appendTo("#output");
+
+  // Output a list of all of the cards that the member
+  // is assigned to
+  Trello.get("members/me/cards", function(cards) {
+      $cards.empty();
+      $.each(cards, function(ix, card) {
+          $("<a>")
+          .attr({href: card.url, target: "trello"})
+          .addClass("card")
+          .text(card.name)
+          .appendTo($cards);
+      });
+  });
 };
 
 var updateLoggedIn = function() {
